a6121109a822ecb567edc852fde173f374f0b1e1
FLD-TN <duyhsne@gmail.com>
2025-03-07 20:13:34 +0700

    fix chức năng quản lí đơn hàng và thanh toán

--- Model/OrderModel.js
+++ Model/OrderModel.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose');
 const orderSchema = new mongoose.Schema({
     userEmail: { type: String, required: true },
     products: [{
-        productID: String,
-        productName: String,
-        productPrice: Number,
-        quantity: Number
+        productID: { type: String, required: true }, // Tham chiếu đến productID trong Product collection
+        productName: { type: String, required: true },
+        productPrice: { type: Number, required: true },
+        productImage: { type: String }, // Có thể null
+        quantity: { type: Number, required: true, min: 1 } // Thêm trường quantity, bắt buộc và lớn hơn 0
     }],
     totalPrice: { type: Number, required: true },
     paymentMethod: { type: String, required: true },
