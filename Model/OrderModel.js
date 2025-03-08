9428e68ee12d28bd72af2bd92508d35806dd9309
FLD-TN <duyhsne@gmail.com>
2025-03-08 18:45:52 +0700

    thêm trường userName cho Order

--- Model/OrderModel.js
+++ Model/OrderModel.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const orderSchema = new mongoose.Schema({
     orderID: { type: String, unique: true }, // Thêm orderID
     userEmail: { type: String, required: true },
+    userName: { type: String, required: true },
     products: [{
         productID: { type: String, required: true },
         productName: { type: String, required: true },
